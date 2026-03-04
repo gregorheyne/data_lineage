227a929dc64ab59124562d491b346f08dc6547d8
gregorheyne <gh.heyne@gmail.com>
2026-03-04 21:45:56 +0100

    dev prod distinction for db connection

--- streamlit_app/utils/db_connections.py
+++ streamlit_app/utils/db_connections.py
@@ -1,4 +1,4 @@
-import pyodbc
+import os
 
 SCHEMA_NAME = "schema_name"
 TABLE_NAME = "table_name"
@@ -11,14 +11,31 @@ DB_CONFIG = {
     "password": "your_password",
 }
 
+PROD_DB_CONFIG = {
+    "server": "your_server.database.windows.net",
+    "database": "your_database",
+}
 
-def get_azure_db_connection() -> pyodbc.Connection:
-    connection_string = (
-        f"DRIVER={{{DB_CONFIG['driver']}}};"
-        f"SERVER=tcp:{DB_CONFIG['server']};"
-        f"DATABASE={DB_CONFIG['database']};"
-        f"UID={DB_CONFIG['username']};"
-        f"PWD={DB_CONFIG['password']};"
-        f"Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-    )
-    return pyodbc.connect(connection_string)
+
+def get_azure_db_connection():
+    env = os.environ.get("APP_ENVIRONMENT", "dev")
+
+    if env == "prod":
+        import pymssql
+        return pymssql.connect(
+            server=PROD_DB_CONFIG["server"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASSWORD"],
+            database=PROD_DB_CONFIG["database"],
+        )
+    else:
+        import pyodbc
+        connection_string = (
+            f"DRIVER={{{DB_CONFIG['driver']}}};"
+            f"SERVER=tcp:{DB_CONFIG['server']};"
+            f"DATABASE={DB_CONFIG['database']};"
+            f"UID={DB_CONFIG['username']};"
+            f"PWD={DB_CONFIG['password']};"
+            f"Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+        )
+        return pyodbc.connect(connection_string)
