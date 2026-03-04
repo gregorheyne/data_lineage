94c3c5736c604272a160ffcb37d336255904d4c2
gregorheyne <gh.heyne@gmail.com>
2026-03-04 10:43:47 +0100

    added requirements.sh file

---
+++ streamlit_app/requirements.sh
@@ -0,0 +1,11 @@
+# Shell file to install Python dependencies for the streamlit app.
+# Run with: bash streamlit_app/requirements.sh
+
+pip install streamlit==1.52.2
+pip install streamlit-aggrid
+pip install pandas
+pip install PyYAML
+pip install pyodbc
+pip install networkx
+pip install graphviz==0.21
+pip install sqlglot==27.29.0
